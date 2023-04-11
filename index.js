b3db67e40eed5c500640b3b4cf221948fd5350f8
Josiah Teya <teyajosiah02@gmail.com>
2023-04-11 12:25:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,82 @@
 /* Your Code Here */
 
+function createEmployeeRecord(employeeData) {
+    const [firstName, familyName, title, payPerHour] = employeeData;
+    return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  
+  function createEmployeeRecords(employeesData) {
+    return employeesData.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+      type: "TimeIn",
+      date,
+      hour: parseInt(hour, 10),
+    });
+    return this;
+  }
+  
+  function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      date,
+      hour: parseInt(hour, 10),
+    });
+    return this;
+  }
+  
+  function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find(
+      (event) => event.date === date
+    );
+    const timeOutEvent = this.timeOutEvents.find(
+      (event) => event.date === date
+    );
+    return (timeOutEvent.hour - timeInEvent.hour) / 100;
+  }
+  
+  function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+  }
+  
+//   function allWagesFor() {
+//     const eligibleDates = this.timeInEvents.map(function (e) {
+//       return e.date;
+//     });
+  
+//     const payable = eligibleDates.reduce(
+//       function (memo, d) {
+//         return memo + wagesEarnedOnDate.call(this, d);
+//       }.bind(this),
+//       0
+//     );
+  
+//     return payable;
+//   }
+  
+  function findEmployeeByFirstName(employees, firstName) {
+    return employees.find((employee) => employee.firstName === firstName);
+  }
+  
+  function calculatePayroll(employees) {
+    return employees.reduce(
+      (totalPay, employee) => totalPay + allWagesFor.call(employee),
+      0
+    );
+  }
+    
+  
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
